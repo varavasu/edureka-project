6e4ba3bb3e177cea134843d4cc856d2a60a14841
Vara <69556528+varavasu@users.noreply.github.com>
2023-06-12 20:59:45 +1000

    Update Simple.java

--- Simple.java
+++ Simple.java
@@ -2,6 +2,6 @@
 
 class Simple{
    public static void main(String[] args) {
-      System.out.println("Welcome to Jenkins's online Training!!!");
+      System.out.println("Welcome to Edureka Jenkins's online Training!!!");
    }
 }
